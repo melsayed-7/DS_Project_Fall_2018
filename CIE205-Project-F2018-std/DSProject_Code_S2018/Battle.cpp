18d22b8d7158db51eba42e82b3199ac0084a3c5b
Khaled <khaledusama1998@gmail.com>
2018-12-01 20:30:20 +0200

    inactive queue function

--- CIE205-Project-F2018-std/DSProject_Code_S2018/Battle.cpp
+++ CIE205-Project-F2018-std/DSProject_Code_S2018/Battle.cpp
@@ -15,7 +15,7 @@ void Battle::AddEnemy(Enemy* Ptr)
 {
 	if (EnemyCount < MaxEnemyCount)
 	{
-		if(Ptr!= nullptr)
+		if (Ptr != nullptr)
 			BEnemiesForDraw[EnemyCount++] = Ptr;
 	}
 
@@ -69,47 +69,27 @@ int Battle::compute_priority(Enemy* ptr)
 
 
 
-
-
-//This is just a demo function for project introductory phase
-//It should be removed in phases 1&2
-void Battle::phase1_simulation()
+Queue <Enemy*>* Battle::fill_inactivelist(Queue <int*>* Data)
 {
-	Queue<int*>*Data = new  Queue<int*>;
+	int enemy_id;
+	int type;
+	int arrival_time;
+	int health;
+	int pow;
+	int reload;
+	int region;
+	int*dummyarr;
+	// Accessing Enemey Classes
+	Fighter * fighter;
+	Healer * healer;
+	Freezer * freezer;
 
-	Data = get_file_2_queue();//enter the name of the file here;
-
-	int *dummyarr = Data->deque();//the first line has the tower data
-
-	double TH = dummyarr[0];//tower health
-	int max_enemies = dummyarr[1];//number of enemies attacked per second
-	double TP = dummyarr[2];//tower power
-
-	Queue <Enemy*>*killed_enemies = new Queue <Enemy*>;//a queue that holds pointer to enemies
-
-	set_initlailized_castle(TH, max_enemies, TP); //Intialize castle parameters using first line of input file
-
-
-		int total_enemies = Data->getsize(); // Get The total number of Enemies to use later
-
-		int enemy_id;
-		int type;
-		int arrival_time;
-		int health;
-		int pow;
-		int reload;
-		int region;
-
-		// Accessing Enemey Classes
-		Fighter * fighter;
-		Healer * healer;
-		Freezer * freezer;
-
-		Queue <Enemy*>*inactive_enemies = new Queue <Enemy*>;//a queue that holds a pointer to enemies
-		int sum = Data->getsize();
-		for (int i = 0; i < Data->getsize(); i++)//fill inactive list
+	Queue <Enemy*>*inactive_enemies = new Queue <Enemy*>;//a queue that holds a pointer to enemies
+	{
+		int intialsize = Data->getsize();
+		for (int i = 0; i < intialsize; i++)//fill inactive list
 		{
-			dummyarr = Data->deque();//the first line has the tower data
+			dummyarr = Data->deque();
 			// Set the Tower parameters
 			enemy_id = dummyarr[0];
 			type = dummyarr[1];
@@ -142,34 +122,51 @@ void Battle::phase1_simulation()
 			case 1://fighter
 				fighter = new Fighter(FIGHTER_CLR, enemy_id, arrival_time, health, pow, reg, 60, 3);
 				inactive_enemies->enque(fighter);
-				
+
 				break;
 			case 2://healer
 				healer = new Healer(HEALER_CLR, enemy_id, arrival_time, health, pow, reg, 60, 3);
 				inactive_enemies->enque(healer);
-				
+
 				break;
 			case 3://freezer
 				freezer = new Freezer(FREEZER_CLR, enemy_id, arrival_time, health, pow, reg, 60, 3);
 				inactive_enemies->enque(freezer);
-				
+
 				break;
 			}
-			
+
 		}
+	}
+	return inactive_enemies;
+}
 
+//This is just a demo function for project introductory phase
+//It should be removed in phases 1&2
+void Battle::phase1_simulation()
+{
+	Queue<int*>*Data = new  Queue<int*>;
 
-	
+	Data = get_file_2_queue();//enter the name of the file here(optional);
+
+	int *dummyarr = Data->deque();//remove the first line has the tower data 
+	double TH = dummyarr[0];//tower health
+	int max_enemies = dummyarr[1];//number of enemies attacked per second
+	double TP = dummyarr[2];//tower power
+	int total_enemies = Data->getsize(); // Get The total number of Enemies to use later
+
+	set_initlailized_castle(TH, max_enemies, TP); //Intialize castle parameters using first line of input file
+
+// intialize queues
+	Queue <Enemy*>*inactive_enemies = fill_inactivelist(Data);//a queue that holds a pointer to enemies
+	Queue <Enemy*>*killed_enemies = new Queue <Enemy*>;//a queue that holds pointer to enemies
 
 	std::cout << "\nWelcome to Castle Battle:\n";
 	std::cout << "\nIn phase2, you will be asked to select game mode\n";
 	std::cout << "\nFor now just press ENTER key to continue...";
 
-	//char tmp[10];
-	//std::cin.getline(tmp, 10);
-
 	double total_tower_health = BCastle.get_total_tower_health();
-	
+
 	GUI * pGUI = new GUI;
 
 
@@ -183,14 +180,15 @@ void Battle::phase1_simulation()
 		Heap1[i] = new Heap<Enemy*>(total_enemies);
 		Heap2[i] = new Heap<Enemy*>(total_enemies);
 	}
-	
+
 	pGUI->PrintMessage("this is phase one simulation");
 
 	int current_tick = 0;
 
-	Heap<Enemy*>** current_heap= Heap1;
-	Heap<Enemy*>** to_be_filled_heap= Heap2;
+	Heap<Enemy*>** current_heap = Heap1;
+	Heap<Enemy*>** to_be_filled_heap = Heap2;
 	Heap<Enemy*>** temp_heap;
+
 	int region_index;
 	int enemey_priority;
 	bool activationflag = true;
@@ -213,7 +211,7 @@ void Battle::phase1_simulation()
 			if (inactive_enemies->front()->get_arraival_time() == current_tick)
 			{
 				region_index = inactive_enemies->front()->GetRegion();
-				enemey_priority=compute_priority(inactive_enemies->front());
+				enemey_priority = compute_priority(inactive_enemies->front());
 				current_heap[region_index]->Enqueue(enemey_priority, inactive_enemies->deque());
 			}
 			else
@@ -223,8 +221,8 @@ void Battle::phase1_simulation()
 			}
 		}
 
-		
-		for (int i = 0; i<4; i++)//one iteration per tower
+
+		for (int i = 0; i < 4; i++)//one iteration per tower
 		{
 			if (current_heap[i]->getcurrent_number() != 0)
 			{
@@ -267,7 +265,7 @@ void Battle::phase1_simulation()
 		temp_heap = current_heap;
 		current_heap = to_be_filled_heap;
 		to_be_filled_heap - temp_heap;
-			
+
 	}
 
 
