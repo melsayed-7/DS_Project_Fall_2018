83df6f72e5ef2c5edb7db9e822959624972e13ea
moustafa-7 <moustafasleem777@gmail.com>
2018-12-30 21:53:37 +0200

    Update Battle.cpp

--- CIE205-Project-F2018-std/DSProject_Code_S2018/Battle.cpp
+++ CIE205-Project-F2018-std/DSProject_Code_S2018/Battle.cpp
@@ -264,109 +264,109 @@ void Battle::phase2_simulation()
 			}
 		}
 
-<<<<<<< HEAD
-		
-=======
-		current_tick++;
+//		<<<<<< < HEAD
+
+			//====== =
+			current_tick++;
 
 
 
->>>>>>> 934170096bb1e0f9d6a60ac4bd525eb535440725
+		//>>>>>> > 934170096bb1e0f9d6a60ac4bd525eb535440725
 
 
 
 
-		for (int i = 0; i < 4; i++)//one iteration per tower, this loop is made to make the enemies act on the tower
-		{
-			current_heap_number = current_heap[i]->getcurrent_number();
-			if (current_heap_number != 0)
+			for (int i = 0; i < 4; i++)//one iteration per tower, this loop is made to make the enemies act on the tower
 			{
-
-				int current_heap_size = current_heap[i]->getcurrent_number();
-
-				for (int j = 0; j < current_heap_size; j++)
+				current_heap_number = current_heap[i]->getcurrent_number();
+				if (current_heap_number != 0)
 				{
 
-					current_enemy = current_heap[i]->Dequeue();
-					if (current_enemy != nullptr)
+					int current_heap_size = current_heap[i]->getcurrent_number();
+
+					for (int j = 0; j < current_heap_size; j++)
 					{
-<<<<<<< HEAD
-=======
-						to_be_filled_heap[i]->Enqueue(compute_priority(current_enemy), current_enemy);
->>>>>>> 934170096bb1e0f9d6a60ac4bd525eb535440725
-						current_enemy->set_target(BCastle.get_tower(i));
-						current_enemy->take_heap(current_heap[i]);
-						current_enemy->Act();
-						AddEnemy(current_enemy);
-						current_enemy->Move();
-					}
 
-
-					// Printing the tower data
-					tower_1_health = BCastle.get_tower(0)->GetHealth();
-					tower_2_health = BCastle.get_tower(1)->GetHealth();
-					tower_3_health = BCastle.get_tower(2)->GetHealth();
-					tower_4_health = BCastle.get_tower(3)->GetHealth();
-
-
-					string messege = " TH1:(" + to_string(tower_1_health) + ")TH2:(" + to_string(tower_2_health) + ")TH3:(" + to_string(tower_3_health) + ")TH4:(" + to_string(tower_4_health) + ")";
-					string messege2 = "TE1:(" + to_string(to_be_filled_heap[0]->getcurrent_number()) + ")TE2:(" + to_string(to_be_filled_heap[1]->getcurrent_number()) + ")TE3:(" + to_string(to_be_filled_heap[2]->getcurrent_number()) + ")TE4:(" + to_string(to_be_filled_heap[3]->getcurrent_number()) + ")";
-					string messege3 = "TK1:(" + to_string(no_killed_enemies[0]) + ")TK2:(" + to_string(no_killed_enemies[1]) + ")TK3:(" + to_string(no_killed_enemies[2]) + ")TK4:(" + to_string(no_killed_enemies[3]) + ")";
-					string messege4 = "----------------------CT " + to_string(current_tick -1);
-					messege = messege + messege2 + messege3 + messege4;
-
-					pGUI->PrintMessage(messege);
-				}
-
-				// vanisher functions
-				int vanishing_time = current_enemy->get_vanishing_time();
-				if (current_enemy->get_type() == 5) {
-					current_enemy->increment_vanishing_time(); // counter
-					// From tick 0:4, vanisher is visible.
-					if (current_enemy->get_vanishing_time() == 5 + vanishing_time) // From tick 5:9, vanisher is visible.
-						current_enemy->set_visible(0);
-					if (current_enemy->get_vanishing_time() == 10 + vanishing_time) // repeat vanishing and appearing for next ticks.
-					{
-						current_enemy->set_visible(1);
-						current_enemy->set_vanishing_time(0);
-					}
-
-				}
-
-				killed_enemies->enque(current_enemy);
-
-				pGUI->DrawBattle(BEnemiesForDraw, EnemyCount);//we draw in here because an enemy can exist and get killed in the same tick
-				for (int j = 0; j < max_enemies; j++)//this loop kills the enemies
-				{
-					if (to_be_filled_heap[i]->getcurrent_number() > 0 && current_enemy->get_visible() == 1)
-					{
-						to_be_hit_enemies[j] = to_be_filled_heap[i]->Dequeue();
-
-						if (to_be_hit_enemies[j] != nullptr)
-							BCastle.tower_act(i, to_be_hit_enemies[j], current_tick);
-
-						if (to_be_hit_enemies[j] != nullptr)
+						current_enemy = current_heap[i]->Dequeue();
+						if (current_enemy != nullptr)
 						{
-							if (to_be_hit_enemies[j]->is_killed())
+							//<<<<<<< HEAD
+							//=======
+							to_be_filled_heap[i]->Enqueue(compute_priority(current_enemy), current_enemy);
+							//>>>>>>> 934170096bb1e0f9d6a60ac4bd525eb535440725
+							current_enemy->set_target(BCastle.get_tower(i));
+							current_enemy->take_heap(current_heap[i]);
+							current_enemy->Act();
+							AddEnemy(current_enemy);
+							current_enemy->Move();
+						}
+
+
+						// Printing the tower data
+						tower_1_health = BCastle.get_tower(0)->GetHealth();
+						tower_2_health = BCastle.get_tower(1)->GetHealth();
+						tower_3_health = BCastle.get_tower(2)->GetHealth();
+						tower_4_health = BCastle.get_tower(3)->GetHealth();
+
+
+						string messege = " TH1:(" + to_string(tower_1_health) + ")TH2:(" + to_string(tower_2_health) + ")TH3:(" + to_string(tower_3_health) + ")TH4:(" + to_string(tower_4_health) + ")";
+						string messege2 = "TE1:(" + to_string(to_be_filled_heap[0]->getcurrent_number()) + ")TE2:(" + to_string(to_be_filled_heap[1]->getcurrent_number()) + ")TE3:(" + to_string(to_be_filled_heap[2]->getcurrent_number()) + ")TE4:(" + to_string(to_be_filled_heap[3]->getcurrent_number()) + ")";
+						string messege3 = "TK1:(" + to_string(no_killed_enemies[0]) + ")TK2:(" + to_string(no_killed_enemies[1]) + ")TK3:(" + to_string(no_killed_enemies[2]) + ")TK4:(" + to_string(no_killed_enemies[3]) + ")";
+						string messege4 = "----------------------CT " + to_string(current_tick - 1);
+						messege = messege + messege2 + messege3 + messege4;
+
+						pGUI->PrintMessage(messege);
+					}
+
+					// vanisher functions
+					int vanishing_time = current_enemy->get_vanishing_time();
+					if (current_enemy->get_type() == 5) {
+						current_enemy->increment_vanishing_time(); // counter
+						// From tick 0:4, vanisher is visible.
+						if (current_enemy->get_vanishing_time() == 5 + vanishing_time) // From tick 5:9, vanisher is visible.
+							current_enemy->set_visible(0);
+						if (current_enemy->get_vanishing_time() == 10 + vanishing_time) // repeat vanishing and appearing for next ticks.
+						{
+							current_enemy->set_visible(1);
+							current_enemy->set_vanishing_time(0);
+						}
+
+					}
+
+					killed_enemies->enque(current_enemy);
+
+					pGUI->DrawBattle(BEnemiesForDraw, EnemyCount); //we draw in here because an enemy can exist and get killed in the same tick
+					for (int j = 0; j < max_enemies; j++)//this loop kills the enemies
+					{
+						if (to_be_filled_heap[i]->getcurrent_number() > 0 && current_enemy->get_visible() == 1)
+						{
+							to_be_hit_enemies[j] = to_be_filled_heap[i]->Dequeue();
+
+							if (to_be_hit_enemies[j] != nullptr)
+								BCastle.tower_act(i, to_be_hit_enemies[j], current_tick);
+
+							if (to_be_hit_enemies[j] != nullptr)
 							{
-								to_be_hit_enemies[j]->set_KTS(current_tick - 1);
-								to_be_hit_enemies[j]->set_KD((current_tick - 1 - to_be_hit_enemies[j]->get_tfirst_shot()));
-								to_be_hit_enemies[j]->set_LT();
-								killed_enemies->enque(to_be_hit_enemies[j]);
-								to_be_hit_enemies[j] = nullptr;
-								no_killed_enemies[j]++;
-								//current_heap[i]->Enqueue(0,nullptr);
-							}
-							else
-							{
-								current_heap[i]->Enqueue(compute_priority(to_be_hit_enemies[j]), to_be_hit_enemies[j]);
+								if (to_be_hit_enemies[j]->is_killed())
+								{
+									to_be_hit_enemies[j]->set_KTS(current_tick - 1);
+									to_be_hit_enemies[j]->set_KD((current_tick - 1 - to_be_hit_enemies[j]->get_tfirst_shot()));
+									to_be_hit_enemies[j]->set_LT();
+									killed_enemies->enque(to_be_hit_enemies[j]);
+									to_be_hit_enemies[j] = nullptr;
+									no_killed_enemies[j]++;
+									//current_heap[i]->Enqueue(0,nullptr);
+								}
+								else
+								{
+									current_heap[i]->Enqueue(compute_priority(to_be_hit_enemies[j]), to_be_hit_enemies[j]);
+								}
 							}
 						}
 					}
 				}
-			}
 
-		}
+			}
 
 		int size_khaled = killed_enemies->getsize();
 
@@ -423,8 +423,8 @@ void Battle::phase2_simulation()
 	//delete pGUI;
 
 
-
-<<<<<<< HEAD
+}
+//<<<<<<< HEAD
 
 
 
@@ -446,12 +446,4 @@ void Battle::phase2_simulation()
 
 <<<<<<< HEAD
 	*/
-}
-=======
-	
-	}
-
-
-
->>>>>>> 934170096bb1e0f9d6a60ac4bd525eb535440725
 
